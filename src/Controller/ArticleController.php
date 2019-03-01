63d76992c19db35791808f38d6ceb66a1b24aede
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-03-01 11:14:46 +0100

    #master Setup inicial del proyecto doctrine.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,11 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
-use App\Repository\ArticleRepository;
 use App\Service\MarkdownHelper;
 use App\Service\SlackClient;
-use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,21 +26,17 @@ class ArticleController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(ArticleRepository $repository)
+    public function homepage()
     {
-        $articles = $repository->findAllPublishedOrderedByNewest();
-
-        return $this->render('article/homepage.html.twig', [
-            'articles' => $articles,
-        ]);
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show(Article $article, SlackClient $slack)
+    public function show($slug, MarkdownHelper $markdownHelper, SlackClient $slack)
     {
-        if ($article->getSlug() === 'khaaaaaan') {
+        if ($slug === 'khaaaaaan') {
             $slack->sendMessage('Kahn', 'Ah, Kirk, my old friend...');
         }
 
@@ -53,22 +46,44 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim.
+
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF;
+
+        $articleContent = $markdownHelper->parse($articleContent);
+
         return $this->render('article/show.html.twig', [
-            'article' => $article,
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
+            'articleContent' => $articleContent,
         ]);
     }
 
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart(Article $article, LoggerInterface $logger, EntityManagerInterface $em)
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
     {
-        $article->incrementHeartCount();
-        $em->flush();
+        // TODO - actually heart/unheart the article!
 
         $logger->info('Article is being hearted!');
 
-        return new JsonResponse(['hearts' => $article->getHeartCount()]);
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
