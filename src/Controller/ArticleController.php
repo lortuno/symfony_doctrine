2f6b5aa04df0e2ece0a1c35f69929addf1e85cb1
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-03-13 10:26:29 +0100

    #master Article use statement

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ArticleController extends AbstractController
 {
