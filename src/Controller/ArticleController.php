6243a292864938f4fb5c3eba3ecea71f5ef11190
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-04-11 17:34:54 +0200

    #master  Genera comentarios y los muestra.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -46,19 +46,11 @@ class ArticleController extends AbstractController {
 			$slack->sendMessage('Kahn', 'Ah, Kirk, my old friend...');
 		}
 
-		$comments = [
-			'I ate a normal rock once. It did NOT taste like bacon!',
-			'Woohoo! I\'m going on an all-asteroid diet!',
-			'I like bacon too! Buy some from my site! bakinsomebacon.com',
-		];
-
-
 		return $this->render(
 			'article/show.html.twig',
 			[
 				'article' => $article,
 				'monty' => $this->getMontyHallTheory(),
-				'comments' => $comments,
 			]
 		);
 	}
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\DataFixtures;
+
 use App\Entity\Article;
+use App\Entity\Comment;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ArticleFixtures extends BaseFixture
-{
-	private static $articleTitles = [
+class ArticleFixtures extends BaseFixture {
+	private static $articleTitles  = [
 		'Why Asteroids are the Subject of this blog',
 		'Life on Planet Mercury: Tan, Relaxing and Fabulous',
 		'Never ever again',
@@ -14,11 +15,22 @@ class ArticleFixtures extends BaseFixture
 		'Big Bang Theory',
 		'Light Speed Travel: Fountain of Youth or Fallacy',
 	];
-	private static $articleImages = [
+
+	private static $articleComments  = [
+		'This blog sucks',
+		'Why are we writing here¡',
+		'Whats the purpose of life',
+		'Titan might have life',
+		'I am gonna miss Sheldon',
+		'Go go power rangers',
+	];
+
+	private static $articleImages  = [
 		'asteroid.jpeg',
 		'mercury.jpeg',
 		'lightspeed.png',
 	];
+
 	private static $articleAuthors = [
 		'Mike Ferengi',
 		'Amy Oort',
@@ -28,24 +40,38 @@ class ArticleFixtures extends BaseFixture
 		'Stephen Hawkings',
 	];
 
-    protected function loadData(ObjectManager $manager)
-    {
-    	$this->createMany(Article::class, 10, function (Article $article, $count) {
-	    $article->setTitle($this->faker->randomElement(self::$articleTitles))
-	            ->setContent($this->faker->paragraphs(5, true)
-	            );
+	protected function loadData(ObjectManager $manager) {
+		$this->createMany(
+			Article::class,
+			10,
+			function (Article $article, $count) use ($manager) {
+				$article->setTitle($this->faker->randomElement(self::$articleTitles))
+				        ->setContent(
+					        $this->faker->paragraphs(5, true)
+				        );
 
-	    // publish most articles
-	    if ($this->faker->boolean(65)) {
-		    $article->setPublishedAt($this->faker->dateTimeBetween('-99 days', '-2 hours'));
-	    }
+				// publish most articles
+				if ($this->faker->boolean(65)) {
+					$article->setPublishedAt($this->faker->dateTimeBetween('-99 days', '-2 hours'));
+				}
 
-	    $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
-	            ->setHeartCount($this->faker->numberBetween(5, 100))
-	            ->setImageFilename($this->faker->randomElement(self::$articleImages));
+				$article->setAuthor($this->faker->randomElement(self::$articleAuthors))
+				        ->setHeartCount($this->faker->numberBetween(5, 100))
+				        ->setImageFilename($this->faker->randomElement(self::$articleImages));
 
-	    });
+				$random = rand(1,5);
+				for ($i=0; $i<$random;$i++)
+				{
+					$genComment = new Comment();
+					$genComment->setAuthorName($this->faker->randomElement(self::$articleAuthors));
+					$genComment->setContent($this->faker->randomElement(self::$articleComments));
+					$genComment->setArticle($article);
+					$manager->persist($genComment);
+				}
 
-        $manager->flush();
-    }
+			}
+		);
+
+		$manager->flush();
+	}
 }
