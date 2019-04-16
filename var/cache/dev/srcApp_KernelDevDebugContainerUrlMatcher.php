b425490de1dc08e2472c3e8c3202a0fb1322d31a
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-04-16 21:32:03 +0200

    #master Añade control de login por contraseña.

--- var/cache/dev/srcApp_KernelDevDebugContainerUrlMatcher.php
+++ var/cache/dev/srcApp_KernelDevDebugContainerUrlMatcher.php
@@ -19,6 +19,7 @@ class srcApp_KernelDevDebugContainerUrlMatcher extends Symfony\Bundle\FrameworkB
             '/' => [[['_route' => 'app_homepage', '_controller' => 'App\\Controller\\ArticleController::homepage'], null, null, null, false, false, null]],
             '/admin/comment' => [[['_route' => 'comment_admin', '_controller' => 'App\\Controller\\CommentAdminController::index'], null, null, null, false, false, null]],
             '/login' => [[['_route' => 'app_login', '_controller' => 'App\\Controller\\SecurityController::login'], null, null, null, false, false, null]],
+            '/logout' => [[['_route' => 'app_logout', '_controller' => 'App\\Controller\\SecurityController::logout'], null, null, null, false, false, null]],
             '/_profiler' => [[['_route' => '_profiler_home', '_controller' => 'web_profiler.controller.profiler::homeAction'], null, null, null, true, false, null]],
             '/_profiler/search' => [[['_route' => '_profiler_search', '_controller' => 'web_profiler.controller.profiler::searchAction'], null, null, null, false, false, null]],
             '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
